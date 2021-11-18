d5582cc19932fb6b84abf05e56bc7f8517e823d2
curious <yangshi55@outlook.com>
2021-11-18 16:33:27 +0800

    1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.sql.SQLOutput;
+
 /**
  * Hello world!
  *
@@ -9,7 +11,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!1" );
-        System.out.println ("test1");
+        System.out.println ("master1");
 
     }
 
