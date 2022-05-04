b29dc1715ac991d831d680e9edcf5f0245bb1d69
curious <yangshi55@outlook.com>
2022-05-04 18:12:52 +0800

    ca

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,9 +10,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!1" );
-        System.out.println ("master1");
 
+    }
+    public String test(){
+        return "123";
     }
 
 }
