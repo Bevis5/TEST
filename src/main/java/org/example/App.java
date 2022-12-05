c7dff9c723dfbd821435d5b72dd9fbb4ab0d8fcc
curious <yangshi55@outlook.com>
2022-12-05 12:10:41 +0800

    1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,6 +11,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println ("changeLocal");
+        System.out.println ("\"stag\" = " + "stag");
+        System.out.println ("\"stag\" = " + "stag");
+
     }
     public String test(){
         return "dev123123123123dev";
---
+++ src/main/java/org/example/test.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @description:
+ **/
+public class test {
+    public static void main(String[] args) {
+        System.out.println ("\"stag\" = " + "stag");
+        System.out.println ("\"stag\" = " + "stag");
+
+    }
+}
